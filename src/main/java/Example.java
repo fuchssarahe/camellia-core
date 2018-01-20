87afce823b400380f7c85ba60d119b0c9863b8d5
Sarah Fuchs <sarah.fuchs@futureadvisor.com>
2018-01-20 08:06:20 -0800

    this actually builds and runs now, even without content

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -11,6 +11,11 @@ public class Example {
 		return "Hello World!";
 	}
 
+	@RequestMapping("/test")
+	String sample() {
+		return "Goodbye world!";
+	}
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
 	}
